96f530ba228855cd1220cb62bb12654f5d690151
cesarParra <cesar.parra@gmail.com>
2024-11-08 11:28:00 -0400

    Introducing 2 views for the articles

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -12,19 +12,51 @@ export interface ArticleWithSlug extends Article {
     slug: string
 }
 
-export function Article({ article }: { article: ArticleWithSlug }) {
-    const formattedDate = new Date(article.date).toLocaleDateString('en-US', {
+function formatDate(date: Date) {
+    return new Date(date).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
     });
+}
+
+export function Article({ article }: { article: ArticleWithSlug }) {
+    return (
+        <article className="md:grid md:grid-cols-4 md:items-baseline">
+            <Card className="md:col-span-3">
+                <CardTitle href={`/articles/${article.slug}`}>
+                    {article.title}
+                </CardTitle>
+                <CardEyebrow
+                    as="time"
+                    dateTime={formatDate(article.date)}
+                    className="md:hidden"
+                    decorate
+                >
+                    {formatDate(article.date)}
+                </CardEyebrow>
+                <CardDescription>{article.description}</CardDescription>
+                <CardCta>Read article</CardCta>
+            </Card>
+            <CardEyebrow
+                as="time"
+                dateTime={formatDate(article.date)}
+                className="mt-1 hidden md:block"
+            >
+                {formatDate(article.date)}
+            </CardEyebrow>
+        </article>
+    )
+}
+
+export function ArticleCondensed({ article }: { article: ArticleWithSlug }) {
     return (
         <Card as="article">
             <CardTitle href={`/blog/${article.slug}`}>
                 {article.title}
             </CardTitle>
-            <CardEyebrow as="time" dateTime={formattedDate} decorate>
-                {formattedDate}
+            <CardEyebrow as="time" dateTime={formatDate(article.date)} decorate>
+                {formatDate(article.date)}
             </CardEyebrow>
             <CardDescription>{article.description}</CardDescription>
             <CardCta>Read article</CardCta>
